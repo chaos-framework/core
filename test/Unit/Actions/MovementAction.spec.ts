8d5e333ffaa6ce344800c1c67e749bfc35cb8221
apeterson <andrew.peterson@us.generaliglobalassistance.com>
2020-12-19 12:53:02 -0500

    Testing world changing action.

---
+++ test/Unit/Actions/MovementAction.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai';
+import 'mocha';
+
+import Entity from '../../../src/EntityComponent/Entity';
+import Room from '../../Mocks/Worlds/Room';
+import { ChangeWorldAction } from '../../../src/Events/Actions/MovementActions';
+
+describe('World Changing Actions', () => {
+  let room1: Room;
+  let room2: Room;
+  let e: Entity;
+
+  beforeEach(() => {
+    room1 = new Room();
+    room2 = new Room();
+    e = new Entity();
+    e._publish(room1, room1.stageLeft);
+  });
+
+  it('Can move entities between worlds', () => {
+    expect(e.world).to.equal(room1);
+    const a = new ChangeWorldAction({target: e, from: room1, to: room2, position: room2.stageLeft});
+    a.execute();
+    expect(e.world).to.equal(room2);
+    expect(e.position.equals(room2.stageLeft)).to.be.true;
+  });
+});
