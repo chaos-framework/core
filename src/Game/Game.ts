db526ee96fff243b23147da98764366e626ffc66
Andrew Peterson <and0@outlook.com>
2021-11-20 12:46:44 -0500

    Client/server changes.

--- src/Game/Game.ts
+++ src/Game/Game.ts
@@ -2,12 +2,14 @@ import { CONNECTION, CONNECTION_RESPONSE } from "../internal";
 
 export interface Game {
   initialize(option: any): void;
+  play(): void;
   onPlayerConnect(msg: CONNECTION): CONNECTION_RESPONSE;
   onPlayerDisconnect(option: any): void;
 }
 
 export function isGame(o: any): o is Game {
   return  typeof o.initialize === 'function' &&
+          typeof o.play === 'function' &&
           typeof o.onPlayerConnect === 'function' &&
           typeof o.onPlayerDisconnect === 'function'
 }
